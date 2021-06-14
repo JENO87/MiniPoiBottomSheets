c08b4f87e23bbd72bb34eb296566d50fbcef36d5
Jens <jens.c.norell@gmail.com>
2021-06-14 17:30:49 +0200

    fixed viewpager and poi

--- app/src/main/java/com/example/minipoibottomsheets/FakeMapFragment.kt
+++ app/src/main/java/com/example/minipoibottomsheets/FakeMapFragment.kt
@@ -15,7 +15,7 @@ class FakeMapFragment : Fragment() {
 
     private var _binding: FragmentFakeMapBinding? = null
     private val binding get() = _binding!!
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var behavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,9 +24,11 @@ class FakeMapFragment : Fragment() {
 
         _binding = FragmentFakeMapBinding.inflate(inflater, container, false).apply {
 
-            bottomSheetBehavior = BottomSheetBehavior.from(miniPoiSheet)
+            behavior = BottomSheetBehavior.from(miniPoiSheet)
+            behavior.isFitToContents = false
+            behavior.halfExpandedRatio = 0.3f
 
-            bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
                     // Handle onslide
                 }
@@ -36,11 +38,11 @@ class FakeMapFragment : Fragment() {
             })
 
             btnBottomSheetPersistent.setOnClickListener {
-                when (bottomSheetBehavior.state) {
-                    BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
-                    BottomSheetBehavior.STATE_HALF_EXPANDED -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-                    BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
-                    else -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                when (behavior.state) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                    BottomSheetBehavior.STATE_EXPANDED -> behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+                    else -> behavior.state = BottomSheetBehavior.STATE_EXPANDED
                 }
             }
 
